79f71cb9700a427b9f3bce295bc690c7a5a7b7d1
Albert-Jan Schot <appie@digiwijs.nl>
2021-03-04 18:28:58 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,2 @@
-m365 status
-m365 login --authType identity --userName 07a6497a-14a5-446d-bb71-03796372117c --verbose
-m365 status
+m365 login --authType identity --userName 07a6497a-14a5-446d-bb71-03796372117c 
+m365 spo site list
