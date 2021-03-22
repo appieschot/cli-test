089857dccebbda3ed76115e3c31fc6c1ced64049
Albert-Jan Schot <appie@digiwijs.nl>
2021-03-22 17:29:47 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 m365 login --authType identity --userName 07a6497a-14a5-446d-bb71-03796372117c 
-m365 spo userprofile set --userName 'albert.jan.schot@digiwijs.nl' --propertyName 'AboutMe' --propertyValue 'Working as a Microsoft 365 developer'
+m365 spo site classic list --output json
