507b5a706c33ffe43f45bb0bb2d6f3b8f942a9b6
Albert-Jan Schot <appie@digiwijs.nl>
2022-03-30 09:11:07 +0200

    Added new sample

---
+++ demo4.sh
@@ -0,0 +1,12 @@
+m365 login --authType identity --userName $1
+externalUsers=`m365 spo user list --webUrl $2 --output json --query "[?contains(LoginName,'#ext#')]"`
+
+
+
+for externalUser in `echo $externalUsers | jq -c '.[]'`; do
+
+    externalUserMail=`echo $user | jq '.Email'`
+
+    m365 aad user hibp --userName $externalUserMail --apiKey $3 --verbose
+
+done
