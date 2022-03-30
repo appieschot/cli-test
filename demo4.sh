010848e601f1390e1658d54834065ef7601e7bae
Albert-Jan Schot <appie@digiwijs.nl>
2022-03-30 09:27:45 +0200

    Fixes

--- demo4.sh
+++ demo4.sh
@@ -1,7 +1,8 @@
+curl -L -o /usr/bin/jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 && \
+  chmod +x /usr/bin/jq
+
 m365 login --authType identity --userName $1
 externalUsers=`m365 spo user list --webUrl $2 --output json --query "[?contains(LoginName,'#ext#')]"`
-
-
 
 for externalUser in `echo $externalUsers | jq -c '.[]'`; do
 
