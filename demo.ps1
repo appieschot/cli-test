bb1a1f79b23ef757ab4b12cf20bc5c155a9431c9
Albert-Jan Schot <appie@digiwijs.nl>
2023-05-04 09:48:33 +0200

    [chore] fixed demo sample

--- demo.ps1
+++ demo.ps1
@@ -1,5 +1,11 @@
-m365 cli config set --key showSpinner --value false
+m365 cli config set --key showSpinner --value $false
 # m365 login --authType identity --userName $env:M365_USER
 
-Write-Host "Hello World"
-Write-Host "Hello World" + $env:M365_USER
+try {
+    Write-Host "Hello World" + $1
+    Write-Host "Hello World" + $env:M365_USER
+}
+catch {
+    Write-Host "Error: $_"
+    exit 1
+}
