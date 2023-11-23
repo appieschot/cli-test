87c76d35cd4a6e3c93157b05ec3de3f915a9f023
Albert-Jan Schot <appie@digiwijs.nl>
2023-11-23 09:47:07 +0100

    [chore] fix for last releases

--- demo3.sh
+++ demo3.sh
@@ -1,4 +1,4 @@
-m365 cli config set --key showSpinner --value false &>/dev/null
+m365 setup --scripting --output none
 
 m365 login --authType identity --userName $1
 m365 spo user list --webUrl $2 --output json --query "[?contains(LoginName,'#ext#')]"
