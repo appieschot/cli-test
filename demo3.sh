239b92386cb56fc1f301bb18962e76c84ad2e9ba
Albert-Jan Schot <appie@digiwijs.nl>
2023-11-23 09:50:04 +0100

    Update demo3.sh

--- demo3.sh
+++ demo3.sh
@@ -1,5 +1,5 @@
 m365 setup --scripting --output none
 
-m365 login --authType identity --userName $1
+m365 login --authType identity --userName $1 &>/dev/null
 m365 spo user list --webUrl $2 --output json --query "[?contains(LoginName,'#ext#')]"
 
